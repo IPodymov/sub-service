b268569d52723a872fcb80c6ce7441d5a282f5a7
Ivan Podymov <podymovivan74@gmail.com>
2026-02-23 15:16:41 +0300

    initialize project structure with configuration files and basic assets

---
+++ assets/stimulus_bootstrap.js
@@ -0,0 +1,5 @@
+import { startStimulusApp } from '@symfony/stimulus-bundle';
+
+startStimulusApp();
+// register any custom, 3rd party controllers here
+// app.register('some_controller_name', SomeImportedController);
